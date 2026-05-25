da354936471297ec800f029bb3da0b7a2378e483
maul112 <tsukiaka313@gmail.com>
2026-05-25 18:40:40 +0700

    fix employee middleware, add button

---
+++ app/Http/Middleware/IsPegawai.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsPegawai
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check() && Auth::user()->employee_id) {
+            return $next($request);
+        }
+
+        return redirect()->route('home')->with('error', 'Anda tidak memiliki akses ke halaman tersebut.');
+    }
+}
